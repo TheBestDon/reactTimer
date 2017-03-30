de7792dce745a7b6cb637e434567016eb863e0c8
TheBestDon <donatas_bakanas@yahoo.com>
2017-03-31 00:11:09 +0300

    Timer and Countdown components created

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,30 +2,30 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
-  
-    return (
-      <div className="top-bar">
-        <div className="top-bar-left">
-          <ul className="menu">
-            <li className="menu-text">React Timer App</li>
-            <li>
-              <IndexLink to="/timer" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
-            </li>
-            <li>
-              <Link to="/countdown" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="top-bar-right">
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
         <ul className="menu">
           <li className="menu-text">
-              Created by <a href="#" target="_blank">TheBestDon</a>
-              </li>
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+          </li>
         </ul>
-        </div>
       </div>
-    );
-  };
-
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://www.github.com/TheBestDon" target="_blank">TheBestDon</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 module.exports = Nav;
