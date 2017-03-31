a22d8aaa2bf5f984805d10fbd8aad4c9898b9488
TheBestDon <donatas_bakanas@yahoo.com>
2017-03-31 19:11:48 +0300

    timer wire up

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,8 +1,66 @@
 var React = require('react');
+var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started': 
+        this.startTimer();
+        break;
+        case 'stopped':
+        this.setState({count: 0});
+        case 'paused':
+        clearInterval(this.timer)
+        this.timer = undefined;
+        break;
+      }
+    }
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
+  handleSetCountdown: function () {
+     this.setState({
+       countdownStatus: 'started'
+     });
+  }, 
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count+1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
+    }, 1000);
+  },
   render: function () {
-    return <p>Timer.jsx component</p>;
+    var{count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if (countdownStatus !== 'started') {
+        return <Controls countdownStatus={'paused'} onStatusChange={this.handleStatusChange}/>
+      } else {
+        return <Controls countdownStatus={'started'} onStatusChange={this.handleStatusChange}/>
+      }
+    };
+
+    return (
+     <div>
+      <h1 className="page-title">Timer App</h1>
+      <Clock totalSeconds={count}/>
+     {renderControlArea()}
+    </div>  
+    );
   }
 });
 
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -34,5 +34,29 @@ describe('Countdown', () => {
          done();
        }, 3001)
      });
-  });
+
+     it('should pause countdown on pause status', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(3);
+       countdown.handleStatusChange('paused');
+
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(3);
+         expect(countdown.state.countdownStatus).toBe('paused');
+       }, 1001);
+         done();
+       });
+
+       it('should stop countdown on stopped status', (done) => {
+       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+       countdown.handleSetCountdown(3);
+       countdown.handleStatusChange('stopped');
+
+       setTimeout(() => {
+         expect(countdown.state.count).toBe(3);
+         expect(countdown.state.countdownStatus).toBe('stopped');
+       }, 1001);
+         done();
+       });
+     });
 });
